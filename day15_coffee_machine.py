aaf80be765dd944f7a98138a0d5dd0d4d2e62106
Said ZITOUNI <zitouni.sd@gmail.com>
2020-12-12 02:04:29 +0100

    program ready to use

--- day15_coffee_machine.py
+++ day15_coffee_machine.py
@@ -30,13 +30,14 @@ resources = {
     "coffee": 100,
 }
 
-choice = input(" Choose your coffee latte, cappucino or espresso : \t")
+choice = input(" Choose your coffee latte, cappuccino or espresso : \t")
 quarter = int(input("how many quarter? \t"))
 dime = int(input("how many dime? \t"))
 nickel = int(input("how many nickel? \t"))
 penny = int(input("how many penny? \t"))
 
-def remaining_money(penny,nickle,dime,quarter,cost=MENU[choice]["cost"]):
+
+def remaining_money(penny,nickel,dime,quarter,cost=MENU[choice]["cost"]):
     total = penny * 0.01 + nickel * 0.05 + quarter * 0.25 + dime * 0.10
     return total - cost
 
@@ -50,14 +51,33 @@ def resources_update(choice):
         elif choice == "latte":
             resources["water"] -= MENU[choice]["ingredients"]["water"] 
             resources["coffee"] -= MENU[choice]["ingredients"]["coffee"]
-            resources["milk"] -= MENU[choice]["ingredients"]["milk"] 
-        elif choice == "cappuccino"
+            resources["milk"] -= MENU[choice]["ingredients"]["milk"]
+        elif choice == "cappuccino":
             resources["water"] -= MENU[choice]["ingredients"]["water"] 
             resources["coffee"] -= MENU[choice]["ingredients"]["coffee"]
             resources["milk"] -= MENU[choice]["ingredients"]["milk"] 
+        elif choice == 'report':
+            for key,value in resources.items():
+                print(f"there is {value} ml left of {key}")
+        else:
+            for key,value in resources.items():
+                print(f"there is {value} ml left of {key}")
+    else: 
+        print("there is not enough ressources! please contact admin!")
+    
     
 
 
+def coffee_machine(choice):
+    resources_update(choice)
+    try:
+        if remaining_money(penny,nickel,dime,quarter,cost=MENU[choice]["cost"]) >=0:
+            print("here is your {} enjoy ☕".format(choice))
+            print("here is your remaining money", remaining_money(penny,nickel,dime,quarter,cost=MENU[choice]["cost"]),"$")
+    except KeyError:
+        print("Your choice is not part of our menu")
+    else:
+        print("you did not insert enough money! please insert more coin")
 
-def coffee_machine(choice,money):
-    
+
+coffee_machine(choice)
