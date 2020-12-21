d2faf6e277da7b142811545489ccd22adadf603d
Said ZITOUNI <zitouni.sd@gmail.com>
2020-12-21 21:11:49 +0100

    Turtle Race very nice game

--- day19_TurtleRace.py
+++ day19_TurtleRace.py
@@ -2,12 +2,58 @@ from turtle import Turtle, Screen
 import random
 
 
+# common main setup
 screen = Screen()
 screen.setup(width=500,height=400)
-screen.textinput(title="Make your bet",prompt="which turtle will win the race: ")
-colors = ["red","green","blue","orange","purple","pink","yellow","cyan","royal blue","orange red","gold","medium violet red","turquoise"]
+bet = screen.textinput(title="Make your bet",prompt="which turtle will win the race: ")
+colors = ["red","green","blue","orange","purple","pink","yellow"]
+def shape_color(turtle):
+    # turtle.color(random.choice(colors))
+    turtle.shape("turtle")
+    turtle.pu()
 
-tim = Turtle()
+# Turtle definition
+red = Turtle()
+green = Turtle()
+blue = Turtle()
+orange = Turtle()
+purple = Turtle()
+Turtles = [red,green,blue,orange,purple] 
+# turtle colors and shape
+i = 0 
+for turtle in Turtles:
+    shape_color(turtle)
+    turtle.color(colors[i])
+    i +=1
 
-tim.goto(-200,-150)
+
+# Turtle start position
+red.goto(x=-200,y=-150)
+green.goto(x=-200,y=100)
+blue.goto(x=-200,y=-100)
+orange.goto(x=-200,y=0)
+purple.goto(x=-200,y=50)
+
+
+# turtle moving 
+for i in range(15):
+    for turtle in Turtles:
+        if turtle.xcor() <= 210:
+            turtle.forward(random.randint(10,50))
+fastest = 0
+for a in Turtles:
+    for b in Turtles:
+        if a.xcor() > b.xcor() and a.xcor() > fastest : 
+                fastest = a.xcor()
+for turtle in Turtles:
+    if turtle.xcor() == fastest:
+        print(f"The winner is {turtle.fillcolor()}")
+        if bet == turtle.fillcolor():
+            print("You won! congratulations!")
+        else: 
+            print("you lost :( try again!")            
+        
+
 screen.exitonclick()
+
+
