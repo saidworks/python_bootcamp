ef7c54d6f26ece37a45a4c4cae6d01d748ee9038
Said ZITOUNI <zitouni.sd@gmail.com>
2021-01-17 00:12:32 +0100

    made the paddle work

--- day22_pong/main.py
+++ day22_pong/main.py
@@ -1,10 +1,15 @@
 from turtle import Screen,Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800,height=600)
 screen.title("Pong Game")
 screen.bgcolor("black")
 
+paddle_1 = Paddle()
+screen.listen()
+screen.onkey(paddle_1.up,"Up")
+screen.onkey(paddle_1.down,"Down")
 
 
 screen.exitonclick()
---
+++ day22_pong/paddle.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.pu()
+        self.shape("square") 
+        self.color ("white")
+        self.shapesize(5,1)
+        self.goto(350,0)
+        self.pos()
+        self.y = 0
+    def up(self):
+        self.y += 10
+        self.setheading(0)
+        self.goto(350,self.y)
+            
+        
+    def down(self):
+        self.y -= 10
+        self.setheading(0)
+        self.goto(350,self.y)
+                
+
+
