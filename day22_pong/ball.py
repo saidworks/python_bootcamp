2c41d46e9de72c222d645f7a60d35220642b2242
Said ZITOUNI <zitouni.sd@gmail.com>
2021-01-31 00:09:13 +0100

    the ball moves up need to add feature like moving randomly and bouncing back when it hits the wall or a paddle

--- day22_pong/ball.py
+++ day22_pong/ball.py
@@ -1,1 +1,12 @@
-import 
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.shapesize(1,1)
+        self.color("white")
+        self.goto(0,0)
+        self.pu()
+    def move(self):
+        self.goto(self.xcor()+10,self.ycor()+10)    
--- day22_pong/main.py
+++ day22_pong/main.py
@@ -1,19 +1,21 @@
 from turtle import Screen,Turtle
 from paddle import *
 import time
-
+from ball import Ball
 screen = Screen()
 screen.setup(width=800,height=600)
 screen.title("Pong Game")
 screen.bgcolor("black")
 screen.tracer(0)
-# creating the paddles
+# creating the paddles and the ball
 paddle_1 = Paddle()
 paddle_1.xcor = 350
 
 paddle_2 = Paddle()
 paddle_2.goto(-350,0)
 paddle_2.xcor=-350
+
+ball = Ball()
 
 #moving the paddles
 screen.listen()
@@ -25,9 +27,9 @@ screen.onkey(paddle_2.down,"Down")
 
 game_is_on = True
 
-while game_is_on: 
+while game_is_on:
     screen.update()
-
-
+    time.sleep(0.1)
+    ball.move()
 
 screen.exitonclick()
