a1ff0c062edb8dfdee20c1c7b514dc8716f828a9
Said ZITOUNI <zitouni.sd@gmail.com>
2021-03-05 08:05:06 +0100

    add first pseudo code

---
+++ 100DaysOfCode/day24/invitation.py
@@ -0,0 +1,20 @@
+"""pseudo-code:
+1. read from a list of people
+    iterate over it 
+2. replace a specific space holder within a word doc with a name of a person in the list
+3. save as a new word document with person name
+ 
+
+ """
+
+from docx import Document
+
+people_invited= ["Maureen","Ahmed"]
+
+# new document 
+document = Document()
+
+document.add_heading('Wedding invitation')
+
+for name in people_invited:
+    paragraph = document.add_paragraph('We are')
