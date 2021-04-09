05f1eac66677a13feec14b40f2799fe034d268a4
Said <zitouni.sd@gmail.com>
2021-04-09 23:40:52 +0100

    about to finish the quiz for US states need to refactor code and learn more about dataframes

--- 100DaysOfCode/day25/us-states-game-end/app.py
+++ 100DaysOfCode/day25/us-states-game-end/app.py
@@ -14,16 +14,41 @@ turtle.shape(image)
 #     print(x, y)
 # turtle.onscreenclick(get_mouse_click_coor)
 #get answer from user
-answer_state = screen.textinput(title='Guess what is the state', prompt='What is another states name')
+
+
 
 
 #read from csv
+''' 
+    read from csv 
+    compare answer to element from array (row)
+    use coordinates to write on the background image 
+ '''
 states = pd.read_csv("50_states.csv")
-state = states.state
-for value in state.values:
-    if answer_state == value:
-        print("ok")
+guessed_states = []
 
-        
-print(states.state)
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title='Guess what is the state', prompt='What is another states name')
+    for value in states.iterrows():
+        serie = value[1].tolist()
+        if answer_state == "exit":
+            missing_states = []
+            for state in serie:
+                if state not in guessed_states:
+                    missing_states.append(state)
+                    guessed_states.append([i for i in range(50)])
+                    break
+        if answer_state == serie[0]:
+            # define turtle to draw names
+            guessed_states.append(answer_state)
+            t = turtle.Turtle()
+            t.hideturtle()
+            t.pu()
+            t.goto(int(serie[1]), int(serie[2]))
+            t.write(answer_state, align='center')
+
+
+
+
+
 turtle.mainloop()
