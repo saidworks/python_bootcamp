10e70b542e661ed79e1dd050a2fc73849fcff6c2
Said <zitouni.sd@gmail.com>
2021-04-16 23:56:27 +0000

    two screen one from tkinter and one turtle need to work out n the GUI for the app

--- 100DaysOfCode/day26/nato_alphabet/main.py
+++ 100DaysOfCode/day26/nato_alphabet/main.py
@@ -23,8 +23,16 @@ for (index, row) in student_data_frame.iterrows():
 #TODO 1. Create a dictionary in this format:
 import pandas as pd
 df = pd.read_csv("nato_phonetic_alphabet.csv")
-alpahbet_dict = {row[0]:row[1] for row in df.iterrows()}
-print(alpahbet_dict)
+df = pd.DataFrame(df)
+alpahbet_dict = {row[0]:row[1] for key,row in df.iterrows()}
+
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+user_input = input("Please enter your name").strip().upper()
+user_input = list(user_input)
+phonetic = []
+for letter in user_input:
+    for item in alpahbet_dict.keys():
+        if letter == item:
+            phonetic.append(alpahbet_dict[letter])
+print(phonetic)
