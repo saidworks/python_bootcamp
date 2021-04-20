d0611886b698543304391e64efe6c378fe9c9d85
Said <zitouni.sd@gmail.com>
2021-04-20 09:06:02 +0000

    Error in the check button need to be addressed grid need to be worked on and used in the previous project

--- 100DaysOfCode/day27/playground.py
+++ 100DaysOfCode/day27/playground.py
@@ -12,7 +12,7 @@ label.pack()
 
 #Buttons
 def action():
-    print("Do something")
+    print(entry.get())
 
 #calls action() when pressed
 button = Button(text="Click Me", command=action)
@@ -23,7 +23,7 @@ entry = Entry(width=30)
 #Add some text to begin with
 entry.insert(END, string="Some text to begin with.")
 #Gets text in entry
-print(entry.get())
+
 entry.pack()
 
 #Text
--- 100DaysOfCode/day28_Pomodoro/main.py
+++ 100DaysOfCode/day28_Pomodoro/main.py
@@ -1,5 +1,6 @@
 
 from tkinter import *
+import time
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -14,7 +15,12 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def countdown(t):
+    while t:
+        seconds = t * 60
+        time.sleep(1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,11 +30,19 @@ window.config(padx=100,pady=50,bg=YELLOW)
 canvas = Canvas(width=200,height=300,bg=YELLOW,highlightthickness=0)
 tomato_img=PhotoImage(file='tomato.png')
 canvas.create_image(100,120,image=tomato_img)
-canvas.create_text(100,150,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
-#add check button
+canvas.create_text(100,150,text='00:00',fill="white",font=(FONT_NAME,35,"bold"))
+#add title, check and buttons
+title = Label(text="Timer",fg=GREEN,font=(FONT_NAME,30,'bold'),bg=YELLOW,highlightthickness=0)
+title.grid(row=0,column=1)
+check = Canvas(width=40,height=40,bg=YELLOW)
 check_button = PhotoImage(file='icons8-check-box-with-check-48.png')
-canvas.create_image(100,280,image=check_button)
-canvas.pack()
-#need to work using grid method 
+check.create_image(20,20,image=check_button)
+check.grid(row=3,column=1)
+start = Button(text='start',padx=10)
+start.grid(row=2,column=0)
+reset = Button(text='reset',padx=10)
+reset.grid(row=2,column=3)
 
+#need to work using grid method 
+canvas.grid(row=1,column=1)
 window.mainloop()
