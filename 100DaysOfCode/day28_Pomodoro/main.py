9dd7be5a371fbcb53e46e520ef9bb6e72ba8a382
Said <zitouni.sd@gmail.com>
2021-04-21 23:02:30 +0000

    timer work fine need to fix the seconds and minutes display to look like a real watch

--- 100DaysOfCode/day28_Pomodoro/main.py
+++ 100DaysOfCode/day28_Pomodoro/main.py
@@ -14,23 +14,30 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def timer():
+    countdown(10)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-def countdown(t):
-    while t:
-        seconds = t * 60
-        time.sleep(1)
+def countdown(count):
+    mins = count // 60
+    secs = count % 60
+    print('{}:{}'.format(mins, secs))
+    if count >= 0 :
 
+        canvas.itemconfig(canvas_text,text='{}:{}'.format(mins,secs))
+        window.after(1000,countdown,count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro technique")
 window.config(padx=100,pady=50,bg=YELLOW)
+
 #create canvas for tomato image
 canvas = Canvas(width=200,height=300,bg=YELLOW,highlightthickness=0)
 tomato_img=PhotoImage(file='tomato.png')
 canvas.create_image(100,120,image=tomato_img)
-canvas.create_text(100,150,text='00:00',fill="white",font=(FONT_NAME,35,"bold"))
+canvas_text = canvas.create_text(100,150,text='00:00',fill="white",font=(FONT_NAME,35,"bold"))
+
+
 #add title, check and buttons
 title = Label(text="Timer",fg=GREEN,font=(FONT_NAME,30,'bold'),bg=YELLOW,highlightthickness=0)
 title.grid(row=0,column=1)
@@ -38,7 +45,7 @@ check = Canvas(width=40,height=40,bg=YELLOW)
 check_button = PhotoImage(file='icons8-check-box-with-check-48.png')
 check.create_image(20,20,image=check_button)
 check.grid(row=3,column=1)
-start = Button(text='start',padx=10)
+start = Button(text='start',padx=10,command=timer)
 start.grid(row=2,column=0)
 reset = Button(text='reset',padx=10)
 reset.grid(row=2,column=3)
