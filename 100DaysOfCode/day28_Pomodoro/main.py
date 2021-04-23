f90f80443518609650f65a787f3ca5eef26f0bf0
Said <zitouni.sd@gmail.com>
2021-04-23 22:48:12 +0000

    second attempt to use image icon multiple time in gui pomodoro

--- 100DaysOfCode/day28_Pomodoro/main.py
+++ 100DaysOfCode/day28_Pomodoro/main.py
@@ -1,15 +1,17 @@
 
 from tkinter import *
 import time
+import math
+import sspicon
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
+LONG_BREAK_MIN = 1
 repeat = 0
 # ---------------------------- TIMER RESET ------------------------------- # 
 def reset_timer():
@@ -24,15 +26,17 @@ def timer():
     repeat += 1
     if repeat % 2 == 0 and repeat<8:
         countdown(SHORT_BREAK_SEC)
-        title.config(text="break")
+        title.config(text="break",fg=RED)
     elif repeat % 2 != 0:
         countdown(WORK_SEC)
-        title.config(text="work")
+        title.config(text="Work",fg=GREEN)
     elif repeat % 8 == 0:
         countdown(LONG_BREAK_SEC)
-        title.config(text="Long break")
-
-
+        title.config(text="Long break",fg=PINK)
+    for i in range(math.floor(repeat / 2)):
+        check_2 = Label(text="S", fg="green")
+        check_2.grid(row=3, column=1)
+        print(repeat)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -48,6 +52,11 @@ def countdown(count):
         window.after(1000,countdown,count-1)
     else:
         timer()
+
+
+
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro technique")
@@ -59,14 +68,13 @@ tomato_img=PhotoImage(file='tomato.png')
 canvas.create_image(100,120,image=tomato_img)
 canvas_text = canvas.create_text(100,150,text='00:00',fill="white",font=(FONT_NAME,35,"bold"))
 
-
 #add title, check and buttons
 title = Label(text="Timer",fg=GREEN,font=(FONT_NAME,30,'bold'),bg=YELLOW,highlightthickness=0)
 title.grid(row=0,column=1)
-check = Canvas(width=40,height=40,bg=YELLOW)
+check_1 = Canvas(width=40, height=40, bg=YELLOW)
 check_button = PhotoImage(file='icons8-check-box-with-check-48.png')
-check.create_image(20,20,image=check_button)
-check.grid(row=3,column=1)
+check_1.create_image(20, 20, image=check_button)
+check_1.grid(row=2, column=2)
 start = Button(text='start',padx=10,command=timer)
 start.grid(row=2,column=0)
 reset = Button(text='reset',padx=10,command=reset_timer)
