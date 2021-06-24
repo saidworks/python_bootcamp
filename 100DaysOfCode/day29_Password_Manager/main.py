145fd8766bceae4e6f8923df07577718786abd68
Said <zitouni.sd@gmail.com>
2021-06-24 08:46:48 +0100

    do UI title and logo

--- 100DaysOfCode/day29_Password_Manager/main.py
+++ 100DaysOfCode/day29_Password_Manager/main.py
@@ -9,11 +9,10 @@ window.config(padx=100,pady=50,bg='WHITE')
 canvas = Canvas(width=200,height=300,bg='BLACK',highlightthickness=0)
 logo_img = PhotoImage(file='logo.png')
 canvas.create_image(100,120,image=logo_img)
-canvas_text = canvas.create_text(100,150,text='Password Reminder',fill="white",font=('Times New Roman',35,"bold"))
-canvas.grid(row=1,column=1)
+canvas.grid(row=2,column=1)
 
-
-
+title = Label(text="Password Manager V1",fg='BLACK',font=('New Times Roman',20,'bold'),bg='WHITE',highlightthickness=0)
+title.grid(row=1,column=1)
 
 
 window.mainloop()
