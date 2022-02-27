6c9c01ae68396bfb987875bec5d64978a2bb93dc
Said ZITOUNI <zitouni.sd@gmail.com>
2022-02-26 20:42:59 -0500

    scatter plot with logarithmic scale

--- datacamp/Matplotlib/basic_plots.py
+++ datacamp/Matplotlib/basic_plots.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 year = [1950,1970,1990,2010]
 pop = [2.159,3.692,5.263,6.972]
 
-plt.scatter(S)
+
+plt.scatter(year,pop)
 #plt.plot(year,pop)
+
+# Put the x-axis on a logarithmic scale
+plt.xscale('log')
 plt.show()
